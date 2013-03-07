d4d7ca8a466bc03ff5226315dc034a7f1c188312
Chris Frisina <github@specialorange.org>
2013-03-07 17:02:55 -0500

    home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CATS::Application.routes.draw do
+  get "home/index"
+
   resources :facebook_users
 
 
@@ -66,7 +68,7 @@ CATS::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
