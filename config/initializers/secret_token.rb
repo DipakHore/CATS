cbf3789ee7fe42924c2db1dd49fafd977b8cb5db
Chris Frisina <github@specialorange.org>
2013-03-07 12:54:55 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CATS::Application.config.secret_token = '2dfa5a1dae906a9968a7a2432d50c97260ba7c7e0bc982a6d91fec95b1842fac434c7efb05d4494889e95ac93b66cb86ecdf3016fe9b4932ed6a4b0a24d824c0'
